df782afeb5be10d00776258f5c29feed3b409e8c
toluwalase <toluwalaseonanuga@gmail.com>
2023-02-16 23:39:43 -0800

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 
-   #include <stdio.h>
-
+#include <stdio.h>
+/**
+ * main - prints some text
+ *
+ * return: 0
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
