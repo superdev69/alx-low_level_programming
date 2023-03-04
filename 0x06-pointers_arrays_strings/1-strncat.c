eb2c5ebd0a9b565627c0a2397060a7c84b0f5abb
toluwalase <toluwalaseonanuga@gmail.com>
2023-03-03 21:36:44 -0800

    concatinating with n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatintes two strings with a byte
+ * @dest: copy to
+ * @str: copy from
+ * @n: character to be copied
+ * Return: destination
+ */
+
+char *_strncat(char *dest, chart *str, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b > n && src[b] != '\0')
+	{
+		det[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
