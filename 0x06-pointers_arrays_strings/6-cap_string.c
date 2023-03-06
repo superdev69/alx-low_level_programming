16b26a0305d5e868095417f22aadb691839227b1
toluwalase <toluwalaseonanuga@gmail.com>
2023-03-06 04:45:34 -0800

    task that capitalizes the first letter of a word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - turns all words of a string to capital
+ * @n: the string
+ * Return: capitalized words
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
