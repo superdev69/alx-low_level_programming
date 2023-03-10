1d2ee81b5093840fce5ad8ef5e5e3dadb342251a
toluwalase <toluwalaseonanuga@gmail.com>
2023-03-10 07:38:04 -0800

    get the lenght of string characters

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the lenght of a prefix substring
+ * @s: pointer to a char string that will be checked
+ * @accept: pointer to a substring we check for
+ * Return: no of bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int y;
+
+	while (*s)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (*s == accept)
+			{
+				x++;
+				break;
+			}
+			else if (accept[y + 1] != '\0')
+				return (x);
+		}
+		s++;
+	}
+	return (x);
+}
