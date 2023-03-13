ff68584e93dea947857481da750be49570eac986
toluwalase <toluwalaseonanuga@gmail.com>
2023-03-13 09:51:28 -0700

    task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * help - helps to return natural s.r
+ * @n: number to find the square root 
+ * @a: square root
+ * Return: 0
+ */
+
+int help(int a, int n)
+{
+	if  (a * a == n)
+		return (a);
+	if (a * a < n)
+		return help(++a, n);
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the given integer
+ * Return: always 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (help(n, 0));
+}
