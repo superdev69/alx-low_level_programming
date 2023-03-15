1e1e43f42f8325d8640bc7740d2e777976685529
toluwalase <toluwalaseonanuga@gmail.com>
2023-03-15 13:41:10 -0700

    tak 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * Return: the name of the program
  */
 
-int main(__attribute__((unused)) int argc, char* argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
