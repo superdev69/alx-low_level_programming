7b1d2c67287fe295a45f0e669e583e93a8a3bcba
toluwalase <toluwalaseonanuga@gmail.com>
2023-11-09 20:08:03 -0800

    bit manipulation project

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,35 +11,5 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > sizeof(n) * 8)
 		return (-1);
 	*n &= ~(1 << index);
-	return (1);
+		return (1);
 }
-
-
-5-flip_bit.c
-
-#include "main.h"
-
-/**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
- * @n: first number
- * @m: second number
- *
- * Return: number of bits to change
- */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
-	int a, countbit = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
-
-	for (a = 63; a >= 0; a--)
-	{
-		current = exclusive >> a;
-		if (current & 1)
-			countbit++;
-	}
-
-	return (countbit);
-}
-
